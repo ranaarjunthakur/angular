dbf5077b64ace48842646c881bbc5131f784b506
arjun pratap rana <89635484+ranaarjunthakur@users.noreply.github.com>
2023-02-23 18:28:34 +0530

    Update DSA_Questions.js

--- DSA_Questions.js
+++ DSA_Questions.js
@@ -15,4 +15,21 @@ var cyl = new Cylinder(7, 4);
 console.log('volume =', cyl.Volume().toFixed(4)); =>  volume = 87.9646
 
 
-/////////////////////////////////////////////////////////
+////////////////////BUBBLE SORT/////////////////////////////////////
+
+var arr = [5,6,3,1,2,4];  
+  
+bubbleSortAlgo(arr);  
+  
+function bubbleSortAlgo(arr){  
+    for(var i=0;i<arr.length;i++){  
+        for(var j=0;j<arr.length-i-1;j++){  
+            if(arr[j]>arr[j+1]){  
+                var tempValue= arr[j];  
+                arr[j]=arr[j+1];  
+                arr[j+1]=tempValue;  
+            }  
+        }  
+    }  
+    console.log(arr);  
+}  
