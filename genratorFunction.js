83f1af767684bda7fcee265c77783a677c62cb25
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2023-06-20 17:16:39 +0530

    Create genratorFunction.js

---
+++ genratorFunction.js
@@ -0,0 +1,32 @@
+///////We can stop and resume this type of functions using yield keyword and denote by (*)//////////
+
+function* getCarBrands() {
+  const americanCarBrands = ['Ford', 'Chevrolet', 'Tesla'];
+  const italianCarBrands = ['Fiat', 'Lancia', 'Alfa Romeo'];
+  const germanCarBrands = ['Volkswagen', 'BMW', 'Mercedes'];
+  yield 1
+  return [...americanCarBrands, ...italianCarBrands, ...germanCarBrands];
+  yield 2
+}
+
+
+let res = getCarBrands()
+console.log(res.next())  => { value: 1, done: false }
+console.log(res.next()) 
+console.log(res.next())   => { value: undefined, done: true }
+
+
+///output//
+{ value: 1, done: false }
+{
+  value: [
+    'Ford',       'Chevrolet',
+    'Tesla',      'Fiat',
+    'Lancia',     'Alfa Romeo',
+    'Volkswagen', 'BMW',
+    'Mercedes'
+  ],
+  done: true
+}
+
+{ value: undefined, done: true }
