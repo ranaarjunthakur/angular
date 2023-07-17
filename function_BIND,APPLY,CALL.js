c315ebf70311389330e6101c0eae7c120ff607a7
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2023-07-17 17:47:44 +0530

    Update function_BIND,APPLY,CALL.js

--- function_BIND,APPLY,CALL.js
+++ function_BIND,APPLY,CALL.js
@@ -1,3 +1,30 @@
+//////////////////////////////////////call()///////////////////////////////////////////////////
+
+function test(arg1, arg2){
+  console.log(this.num, arg1, arg2); // 100, 10, 20
+}
+
+test.call({num: 100}, 10, 20);
+
+                        //apply()//
+
+function test(...arguments){
+  console.log(this.num, arguments);//100, [1,2,3]
+}
+
+test.apply({num: 100}, [1,2,3]); 
+
+                            //Bind()//
+
+function test(arg){
+ console.log(this.number, arg);
+}
+
+let bindedFn = test.bind({number: 99}, "argument");
+
+bindedFn(); // 99, "argument"
+
+
 /////////////////////With the bind() method, an object can borrow a method from another object.//////////////////////
 
 
