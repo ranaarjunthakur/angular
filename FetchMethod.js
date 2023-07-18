ddad1c0500e4658d9d4fe7d98156b67efaa88097
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2023-07-18 12:24:23 +0530

    Create FetchMethod.js

---
+++ FetchMethod.js
@@ -0,0 +1,22 @@
+/////////////The fetch() method accepts two arguments url and options.///////////////
+
+const postData = {
+  title: 'John',
+  body: 'example',
+  userId: 101,
+};
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(postData)
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
